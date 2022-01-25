fb758d9d1196fbd150677d65c030390dee7a6981
kapilushelar@gmail.com <you@example.com>
2022-01-25 22:58:39 +0530

    work

---
+++ 06_queues/Ques_1/main/qu1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
+
+QueueHandle_t sensor_queue;
+
+void Temp_Sensor(void *pvParameters)
+{
+    int temp_send = 15;
+    while(1)
+    {
+        printf("Send Data: Temperature sensed in celcius: %d\n", temp_send);
+        xQueueSend(sensor_queue, &temp_send, portMAX_DELAY);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        temp_send++;
+    }
+}
+
+void Temp_Processor(void *pvParameters)
+{
+    int temp_recv;
+    while(1)
+    {
+        xQueueReceive(sensor_queue, &temp_recv, portMAX_DELAY);
+        printf("Received Data: Temperature in Kelvin: %d\n", temp_recv + 273);
+    }
+}
+
+void app_main()
+{
+    sensor_queue = xQueueCreate(7, sizeof(int));
+    xTaskCreate(Temp_Sensor, "Temp_Sensor", 2048, NULL, 5, NULL);
+    xTaskCreate(Temp_Processor, "Temp_Processor", 2048, NULL, 7, NULL); 
+    printf("Main Task\n");                   
+}
