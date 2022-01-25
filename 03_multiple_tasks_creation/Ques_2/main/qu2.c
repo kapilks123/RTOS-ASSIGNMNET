6f8b92e78ea84dd84debc1138e28d4c62d6d5709
kapilushelar@gmail.com <you@example.com>
2022-01-25 18:26:44 +0530

    work hard

---
+++ 03_multiple_tasks_creation/Ques_2/main/qu2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+
+TaskHandle_t xHandleTask, xHandleTask1, xHandleTask2;
+
+
+
+void Task1(void *pvParameters)
+{
+    while(1)
+    {
+        printf("Task1: Priority = 5\n");
+    }
+}
+
+void Task2(void *pvParameters)
+{
+    while(1)
+    {
+        printf("Task2: Priority = 5\n");
+    }
+}
+void init_task(void *pvParameters)
+{
+    printf("Initial task: Priority = 6\n");
+    xTaskCreate(Task1, "Task1", 2048, NULL, 5, &xHandleTask1);
+    xTaskCreate(Task2, "Task2", 2048, NULL, 5, &xHandleTask2); 
+    printf("Initial task is finishing after creating two tasks with same priorities.\n");
+    vTaskDelete( xHandleTask ); 
+}
+void app_main()
+{
+    xTaskCreate(init_task, "Initial_Task", 2048, NULL, 6, &xHandleTask);                      
+}
